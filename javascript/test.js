523f2ac8c0be7be95f4e20461afca8b127b33376
2201031030049 <161229871+2201031030049@users.noreply.github.com>
2025-12-18 13:46:55 +0000

    test.js config done

---
+++ javascript/test.js
@@ -0,0 +1,17 @@
+const coding = ["ruby","python","cpp","java","js"]
+
+// coding.forEach( function (items){
+//     console.log(items)
+// })
+
+// // coding.forEach ( (items) =>{
+// //     console.log(items);
+    
+// // })
+
+// function printme (items){
+//     console.log(items);
+    
+// }
+
+// coding.foreach(printme)
